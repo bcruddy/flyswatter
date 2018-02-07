57472136dfc87d4be9f65f9712890d89497ed849
Brian Ruddy <briancruddy@gmail.com>
2018-02-06 23:26:34 -0500

    Add support for using flyswatter programmatically (#5)

--- libs/server.js
+++ libs/server.js
@@ -1,27 +1,29 @@
 const http = require('http'),
     https = require('https'),
     express = require('express'),
-    keygen = require('./keygen'),
-    listen = require('./listen');
+    keygen = require('./keygen');
 
 module.exports = function server (config) {
-    const app = express(),
-        {path, https: isHttps} = config;
-    
-    app.use(express.static(path));
+    return new Promise((resolve, reject) => {
+        const app = express(),
+            {path, https: isHttps} = config;
 
-    let server;
-    if (isHttps) {
-        keygen()
-            .then(credentials => {
-                server = https.createServer(credentials, app);
-                listen(server, config);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    } else {
-        server = http.createServer(app);
-        listen(server, config);
-    }
+        app.use(express.static(path));
+
+        let server;
+        if (isHttps) {
+            keygen()
+                .then(credentials => {
+                    server = https.createServer(credentials, app);
+
+                    resolve(server);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        } else {
+            server = http.createServer(app);
+            resolve(server);
+        }
+    });
 };
